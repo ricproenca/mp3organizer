2aad6d5476f2ff550467151a71c38d8af681fe71
Ricardo Proença <ricproenca@gmail.com>
2019-04-22 22:14:51 +0200

    update setup files

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-async function f() {
-  return 1;
+import * as mm from "music-metadata";
+
+async function parseFiles(audioFiles) {
+  for (const audioFile of audioFiles) {
+    // await will ensure the metadata parsing is completed before we move on to the next file
+    const metadata = await mm.parseFile(audioFile);
+    // Do great things with the metadata
+    console.log(metadata.common.grouping);
+    console.log(metadata.common.title);
+    console.log(metadata.common.album);
+    console.log(metadata.common.genre);
+    console.log(metadata.common.year);
+    console.log(metadata.common.artists);
+    console.log(metadata.common.artist);
+    //console.log(metadata.common);
+  }
 }
 
-f().then(() => {
-  console.log("1");
-}); // 1
+parseFiles(["./testfiles/15 Sheryl Crow - All I Wanna Do.mp3"]);
